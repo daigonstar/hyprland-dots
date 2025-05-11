28036a37e2539c107566a17e381343c122ace799
Daigonstar <daigonstar@gmail.com>
2025-05-11 22:57:01 +0100

    initial

---
+++ hyprdots/hyprinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Application variables
+req="hyprland rofi-wayland waybar nwg-displays nwg-look hyprshot swaync libnotify hyprlock hypridle hyprpaper ttf-cascadia-code-nerd pavucontrol playerctl xorg-xwayland wayland-protocols hyprpolkitagent"
+opt="ghostty flatpak buah firefox thunar stow starship plymouth"
+nvidia="nvidia-dkms linux-headers nvidia-utils libva-nvidia-driver"
+
+# Install git and paru
+sudo pacman -Syu --noconfirm git
+
+git clone https://aur.archlinux.org/paru.git
+cd paru || exit
+makepkg -si --noconfirm
+
+echo "Paru now installed."
+
+# Install required packages
+echo "Now installing required packages"
+paru -S --noconfirm $req
+
+
+echo "Would you like the optional packages? ($opt)"
+read -rp "Install optional packages? [y/N]: " install_opt
+if [[ "$install_opt" =~ ^[Yy]$ ]]; then
+  paru -S --noconfirm $opt
+fi
+
+echo "Are you using NVIDIA graphics?"
+read -rp "Install NVIDIA packages? [y/N]: " install_nvidia
+if [[ "$install_nvidia" =~ ^[Yy]$ ]]; then
+  paru -S --noconfirm $nvidia
+fi
+
+
